5e0001a563faab76b465f1a9caf959f6676f4302
pawoo0211 <pawoo0211@naver.com>
2023-05-14 13:47:14 +0900

    [api] Mock API 재설계

--- api/src/main/java/com/techblog/api/controller/PostController.java
+++ api/src/main/java/com/techblog/api/controller/PostController.java
@@ -1,37 +1,22 @@
 package com.techblog.api.controller;
 
 import com.techblog.api.post.in.CollectPostIn;
-import com.techblog.api.post.in.UpdatePostIn;
 import com.techblog.api.post.out.CollectPostOut;
-import com.techblog.api.post.out.DeletePostOut;
 import com.techblog.api.post.out.SearchPostOut;
-import com.techblog.api.post.out.UpdatePostOut;
 import com.techblog.common.CommonResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/posts")
 public class PostController {
 
-    @PostMapping("{company}/{id}")
-    public CommonResponse collectPost(@PathVariable("company") String company, @PathVariable("id") String id,
-                                      @RequestBody CollectPostIn collectPostIn) {
-        return CommonResponse.ok(new CollectPostOut(id));
+    @PostMapping("/collect")
+    public CommonResponse collectPost(@RequestBody CollectPostIn collectPostIn) {
+        return CommonResponse.ok(new CollectPostOut("1"));
     }
 
-    @PutMapping("/{company}/{id}")
-    public CommonResponse updateCollectedPost(@PathVariable("company") String company, @PathVariable("id") String id,
-                                              @RequestBody UpdatePostIn updatePostIn) {
-        return CommonResponse.ok(new UpdatePostOut(id));
-    }
-
-    @DeleteMapping("/{company}/{id}")
-    public CommonResponse deleteCollectedPost(@PathVariable("company") String company, @PathVariable("id") String id) {
-        return CommonResponse.ok(new DeletePostOut(id));
-    }
-
-    @GetMapping("/{keyword}")
-    public CommonResponse searchPost(@PathVariable("keyword") String keyword) {
+    @GetMapping("/search")
+    public CommonResponse searchPost(@RequestParam("keyword") String keyword) {
         return CommonResponse.ok(new SearchPostOut("https://d2.naver.com/home"));
     }
 }
