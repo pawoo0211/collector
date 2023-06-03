873a5dcc781cf8b6c0227110e33349421c300d21
pawoo0211 <pawoo0211@naver.com>
2023-06-03 23:35:28 +0900

    [api] 네이버 기술 블로그 글 모든 URL 추가 #15

--- common/src/main/java/com/techblog/common/constant/Company.java
+++ common/src/main/java/com/techblog/common/constant/Company.java
@@ -10,8 +10,26 @@ import java.util.List;
 @AllArgsConstructor
 public enum Company {
 
-    NAVER("NAVER", Arrays.asList("https://d2.naver.com/api/v1/contents?categoryId=2&page=0&size=20",
-            "https://d2.naver.com/api/v1/contents?categoryId=2&page=1&size=20"));
+    NAVER("NAVER", Arrays.asList(
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=0&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=1&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=2&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=3&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=4&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=5&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=6&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=7&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=8&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=9&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=10&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=11&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=12&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=13&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=14&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=15&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=16&size=20",
+            "https://d2.naver.com/api/v1/contents?categoryId=2&page=17&size=20"
+            ));
 
     private final String name;
     private final List<String> urlList;
