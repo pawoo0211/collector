06129c4ff2a861df59dec9d3a56e7c7e413615a3
pawoo0211 <pawoo0211@naver.com>
2023-06-25 00:09:55 +0900

    [dao] RedisConfig 구현 #24

--- dao/src/main/java/com/techblog/dao/mongodb/PostMongoRepository.java
+++ dao/src/main/java/com/techblog/dao/mongodb/PostMongoRepository.java
@@ -1,6 +1,5 @@
 package com.techblog.dao.mongodb;
 
-import com.techblog.dao.mongodb.PostMongoEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
